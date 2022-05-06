0dba8c172702f2baacca9c4f710d51a0e304a9a8
Lichie <lichie567@protonmail.com>
2022-05-06 04:51:21 -0700

    update to 0.2.3.0

--- XIVAuras/Plugin.cs
+++ XIVAuras/Plugin.cs
@@ -26,7 +26,11 @@ namespace XIVAuras
     {
         public const string ConfigFileName = "XIVAuras.json";
 
+<<<<<<< HEAD
         public static string Version { get; private set; } = "0.2.5.0";
+=======
+        public static string Version { get; private set; } = "0.2.3.0";
+>>>>>>> 6a054bc (update to 0.2.3.0)
 
         public static string ConfigFileDir { get; private set; } = "";
 
